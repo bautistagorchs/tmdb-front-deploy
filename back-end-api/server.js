ff6fdd5259a165edcbd0a48bc48010708681fbf1
bautistagorchs <bautista.gorchs@gmail.com>
2023-10-30 18:39:28 -0300

    deleted files

--- back-end-api/server.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-const morgan = require("morgan");
-
-app.use(express.json());
-app.use(morgan("dev"));
-app.listen(3001, () => {
-  console.log("Server levantado en el 3001");
-});
