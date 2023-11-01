c8eb758892c132ef392f569bb9ca2bb01aaaec90
bautistagorchs <bautista.gorchs@gmail.com>
2023-11-01 00:27:18 -0300

    create components-files

---
+++ front/src/components/Main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./Navbar";
+// import Movies from "./Movies";
+// import TvShows from "./TvShows";
+
+const Main = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+};
+
+export default Main;
--- front/src/components/Navbar.jsx
+++ front/src/components/Navbar.jsx
@@ -1,17 +1,13 @@
 import React from "react";
-import { Link } from "react-router-dom";
+//import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <a href="/">Home</a>
-      <Link to="/movies">
-        <a href="/">Movies</a>
-      </Link>
+      <a href="/">Movies</a>
       <a href="/">Tv Shows</a>
-      <Link to="/login">
-        <button>Log in</button>
-      </Link>
+      {/* <button onClick={requestToAPI}>Log in</button> */}
     </div>
   );
 };
