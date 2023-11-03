c7999c56d0a3e839163a0dbeef441deaded78ada
bautistagorchs <bautista.gorchs@gmail.com>
2023-11-03 19:23:42 -0300

    change Navbar content and styles

--- front/src/components/Navbar.jsx
+++ front/src/components/Navbar.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-//import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
@@ -7,12 +6,14 @@ const Navbar = () => {
   const user = useSelector((state) => state.user);
   return (
     <div className="navbar">
-      <Link to="/main">Home</Link>
-      <Link to="/movies">Movies</Link>
-      <Link to="/">Tv Shows</Link>
+      <div className="links-navbar">
+        <Link to="/main">Home</Link>
+        <Link to="/movies">Movies</Link>
+        <Link to="/">Tv Shows</Link>
+      </div>
       {/* ASK HERE! */}
       {user.id ? (
-        <p>Welcome, ${user.name}</p>
+        <p>Welcome, {user.name}</p>
       ) : (
         <a href="/users/login">Log in</a>
       )}
