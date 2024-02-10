07e1281dff0144a4e4ceff20e5488e482595a3d1
bautistagorchs <bautista.gorchs@gmail.com>
2024-02-10 17:12:21 -0300

    remove back

--- back/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const users = require("./users");
-
-router.use("/users", users);
-router.use("/", (req, res) => res.sendStatus(404));
-
-module.exports = router;
